d5001b17dcb2cbced91c34d6beb7b866466963b6
jasfeng <jasfeng@cisco.com>
2014-03-17 13:47:20 -0700

    token

--- src/main/java/com/example/controller/EventController.java
+++ src/main/java/com/example/controller/EventController.java
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class EventController {
 	
 	 @RequestMapping("event/*https://www.acme-marketplace.com/api/integration/v1/events/{token}")
+	 @ResponseBody
 	 public String EventHandler(@PathVariable("token") String token) {
-		 System.out.println("token is "+token);
-		 return "people";
+		 //System.out.println("token is "+token);
+		 return "token is ";
 	 }
 	 
 }
