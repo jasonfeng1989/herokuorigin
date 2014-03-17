b95008d8a1b4c085a20ada2a44e3da9dd6340347
jasfeng <jasfeng@cisco.com>
2014-03-17 14:22:21 -0700

    t1

--- src/main/java/com/example/controller/EventController.java
+++ src/main/java/com/example/controller/EventController.java
@@ -1,5 +1,10 @@
 package com.example.controller;
 
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -7,16 +12,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class EventController {
-	
-	 @RequestMapping("event/*https://www.acme-marketplace.com/api/integration/v1/events/{token}")
+		
+	 @RequestMapping(value = {"event/", "event/"})
 	 @ResponseBody
-	 public String EventHandler(@PathVariable("token") String token) {
-		 //System.out.println("token is "+token);
-		 return "token is ";
+	 public void EventHandler(@RequestParam Map<String, String> url, HttpServletResponse response) throws IOException {
+		 response.getWriter().println("url is"+url);
 	 }
 	 
 }
