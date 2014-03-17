ed5e8dcfaf3f0f9760a885e0be86414b44e75b45
jasfeng <jasfeng@cisco.com>
2014-03-17 13:31:56 -0700

    add eventcontroller

---
+++ src/main/java/com/example/controller/EventController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class EventController {
+	
+	 @RequestMapping("event/*https://www.acme-marketplace.com/api/integration/v1/events/{token}")
+	 public String EventHandler(@PathVariable("token") String token) {
+		 System.out.println("token is "+token);
+		 return "abc";
+	 }
+	 
+}
