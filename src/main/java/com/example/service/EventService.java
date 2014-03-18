57aeb471f0f1bbf8ea4edb16d42987d5df09a02b
jasfeng <jasfeng@cisco.com>
2014-03-17 18:02:56 -0700

    t4

--- src/main/java/com/example/service/EventService.java
+++ src/main/java/com/example/service/EventService.java
@@ -1,14 +1,16 @@
 package com.example.service;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class EventService {
-	public void FetchEvent(String token){
-		OAuthConsumer consumer = new DefaultOAuthConsumer("test-7940", "UlhALaaoJ20e6caa");
+	public void FetchEvent(String token) throws IOException{
+		//OAuthConsumer consumer = new DefaultOAuthConsumer("test-7940", "UlhALaaoJ20e6caa");
 		URL url = new URL("https://www.appdirect.com/AppDirect/rest/api/events/"+token);
 		HttpURLConnection request = (HttpURLConnection) url.openConnection();
-		consumer.sign(request);
+		//consumer.sign(request);
 		request.connect();
 	}
 }
