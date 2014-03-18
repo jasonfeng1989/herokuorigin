2f4b09c6804e9c399b27ae6f94d292df16674737
Jason <jasonfeng19890915@gmail.com>
2014-03-17 22:52:14 -0700

    move fetch out of service

--- src/main/java/com/example/controller/EventController.java
+++ src/main/java/com/example/controller/EventController.java
@@ -1,31 +1,29 @@
 package com.example.controller;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import com.example.service.EventService;
-import com.example.service.PersonService;
 
-import javax.servlet.http.HttpServletResponse;
-
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.basic.DefaultOAuthConsumer;
 import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class EventController {
 	
-	@Autowired
-    private EventService eventService;
+	//@Autowired
+    //private EventService eventService;
 	
 	/* request url is http://herokuorigin.herokuapp.com/event/create?url=https://www.acme-marketplace.com/api/integration/v1/events/1234
 	 * will read out action = "create"
@@ -37,9 +35,20 @@ public class EventController {
 	 OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException, IOException {
 		 String[] URLParts = url.split("/");
 		 String token = URLParts[URLParts.length-1];
-		 return(eventService.FetchEvent(token));
+		 return(FetchEvent(token));
 		 
 		 //return "url is "+url+"\n cation is "+action+"\n token is "+token;
 	 }
+	 
+	 public String FetchEvent(String token) throws IOException, OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException{
+			OAuthConsumer consumer = new DefaultOAuthConsumer("test-7940", "UlhALaaoJ20e6caa");
+			URL url = new URL("https://www.appdirect.com/AppDirect/rest/api/events/"+token);
+			HttpURLConnection request = (HttpURLConnection) url.openConnection();
+			consumer.sign(request);
+			request.connect();
+			int ResponseCode = request.getResponseCode();
+			String ResponseMessage = request.getResponseMessage();
+			return ResponseMessage;
+	}
 	 
 }
