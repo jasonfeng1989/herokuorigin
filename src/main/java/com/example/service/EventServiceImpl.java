3c77c2eb19b0d2562f0c849350cc89691bb78a9b
jasfeng <jasfeng@cisco.com>
2014-03-19 18:13:48 -0700

    319t24

--- src/main/java/com/example/service/EventServiceImpl.java
+++ src/main/java/com/example/service/EventServiceImpl.java
@@ -97,11 +97,11 @@ public class EventServiceImpl implements EventService {
 		// bind two objects 
 		companySubscription.addUser(user);
 		user.setCompanySubscription(companySubscription);
-		//persistUser(user);
+		persistUser(user);
 		//String accountId = persistCompanySubscription(companySubscription);
-		//Query query = em.createQuery("SELECT cs FROM com.example.model.CompanySubscription cs");
-	    //List<CompanySubscription> cslist = (List<CompanySubscription>) query.getResultList();
-		return companySubscription.getEdition();
+		Query query = em.createQuery("SELECT cs FROM com.example.model.CompanySubscription cs");
+	    List<CompanySubscription> cslist = (List<CompanySubscription>) query.getResultList();
+		return cslist.toString() + companySubscription.getEdition();
 		
 		/*
 		String accountId = user.getCompanySubscription().getCompanyId().toString();
