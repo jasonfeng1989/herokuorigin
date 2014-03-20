cadae066e17642c481449857111ec0d4cbe46508
Jason <jasonfeng19890915@gmail.com>
2014-03-19 20:17:02 -0700

    319t31

--- src/main/java/com/example/service/EventServiceImpl.java
+++ src/main/java/com/example/service/EventServiceImpl.java
@@ -100,7 +100,7 @@ public class EventServiceImpl implements EventService {
 		//persistUser(user);
 		//String accountId = 
 		persistCompanySubscription(companySubscription);
-		Query query = em.createQuery("SELECT u FROM com.example.model.User u");
+		Query query = em.createQuery("SELECT u FROM com.example.model.CompanySubscription u");
 	    List<CompanySubscription> alist = (List<CompanySubscription>) query.getResultList();
 		return alist.toString() + companySubscription.getEdition();
 		
