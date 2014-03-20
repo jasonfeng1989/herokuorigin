1cf95005222b880e0d562cfac0d833bff3245ba2
Jason <jasonfeng19890915@gmail.com>
2014-03-19 21:00:12 -0700

    319t34

--- src/main/java/com/example/service/EventService.java
+++ src/main/java/com/example/service/EventService.java
@@ -6,6 +6,9 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 
+import com.example.model.CompanySubscription;
+import com.example.model.User;
+
 import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
@@ -14,4 +17,7 @@ public interface EventService {
 	public String FetchEvent(String token) throws Exception;
 	public String HandleEvent(Document doc);
 	public String CreateOrder(Document doc);
+	public CompanySubscription CreateCompanySubscription(Document doc);
+	public User CreateUser(Document doc, CompanySubscription companySubscription);
+	public void persistCompanySubscription(CompanySubscription companySubscription);
 }
