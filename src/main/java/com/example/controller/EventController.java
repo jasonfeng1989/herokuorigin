22d94385557b6e8f409497a322bf55fbe934bd7c
jasfeng <jasfeng@cisco.com>
2014-03-21 16:17:27 -0700

    321t23

--- src/main/java/com/example/controller/EventController.java
+++ src/main/java/com/example/controller/EventController.java
@@ -2,6 +2,11 @@ package com.example.controller;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.List;
+
 import com.example.service.EventService;
 
 import oauth.signpost.OAuthConsumer;
@@ -10,12 +15,18 @@ import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
 
+import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.discovery.DiscoveryException;
+import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.message.AuthRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponentsBuilder;
+
 
 @Controller
 public class EventController {
@@ -23,6 +34,8 @@ public class EventController {
 	
 	@Autowired
     private EventService eventService;
+	
+	public final ConsumerManager manager = new ConsumerManager();  
 	
 	/* request url is http://herokuorigin.herokuapp.com/event/create?url=https://www.appdirect.com/rest/api/events/1234
 	 * will read out action = "create"
@@ -41,10 +54,6 @@ public class EventController {
 		 //return "url is "+url+"\n cation is "+action+"\n token is "+token;
 	 }
 	 
-	 @RequestMapping(value="login/")
-	 @ResponseBody
-	 public String LoginHandler(@RequestParam("openid") String openid) {
-		 return openid;
-	 }
+	
 
 }
---
+++ src/main/java/com/example/controller/SecurityOpenIDController.java
@@ -0,0 +1,81 @@
+package com.example.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.openid4java.association.AssociationException;
+import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.consumer.VerificationResult;
+import org.openid4java.discovery.DiscoveryException;
+import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.discovery.Identifier;
+import org.openid4java.message.AuthRequest;
+import org.openid4java.message.AuthSuccess;
+import org.openid4java.message.MessageException;
+import org.openid4java.message.ParameterList;
+import org.openid4java.message.ax.AxMessage;
+import org.openid4java.message.ax.FetchRequest;
+import org.openid4java.message.ax.FetchResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Controller  
+@RequestMapping("openid")
+public class SecurityOpenIDController {
+	public final ConsumerManager manager = new ConsumerManager();  
+	
+	@RequestMapping(value="/login")
+	public void LoginHandler(@RequestParam("openid") String openid, UriComponentsBuilder builder,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String returnUrl = builder.path("/openid/return").build().toUriString();  
+		List discoveries = manager.discover(openid);
+		DiscoveryInformation discovered = manager.associate(discoveries);
+		HttpServletRequest httpReq = null;
+		httpReq.getSession().setAttribute("openid-disc", discovered);
+		AuthRequest authReq = manager.authenticate(discovered, returnUrl);  
+		 
+		FetchRequest fetch = FetchRequest.createFetchRequest();  
+		fetch.addAttribute("email", "http://axschema.org/contact/email", true);  
+	    fetch.addAttribute("firstName", "http://axschema.org/namePerson/first", true);  
+	    fetch.addAttribute("lastName", "http://axschema.org/namePerson/last", true);  
+	    authReq.addExtension(fetch);  
+	     
+		if (!discovered.isVersion2()) {
+			response.sendRedirect(authReq.getDestinationUrl(true)); 
+		} else {
+			response.sendRedirect(authReq.getDestinationUrl(true));  
+		 }
+	 }
+	 
+	@RequestMapping("/return")
+	public Identifier verifyResponse(HttpServletRequest httpReq) throws Exception { 
+		ParameterList response = new ParameterList(httpReq.getParameterMap());
+		DiscoveryInformation discovered = (DiscoveryInformation) httpReq.getSession().getAttribute("openid-disc");  
+		StringBuffer receivingURL = httpReq.getRequestURL();
+        String queryString = httpReq.getQueryString();
+        if (queryString != null && queryString.length() > 0)
+            receivingURL.append("?").append(httpReq.getQueryString());
+        VerificationResult verification = manager.verify(receivingURL.toString(),response, discovered);
+        Identifier verified = verification.getVerifiedId();  
+        if (verified != null) {  
+            AuthSuccess authSuccess = (AuthSuccess) verification.getAuthResponse();  
+            if (authSuccess.hasExtension(AxMessage.OPENID_NS_AX)) {  
+            	FetchResponse fetchResp = (FetchResponse) authSuccess.getExtension(AxMessage.OPENID_NS_AX);  
+            	List emails = fetchResp.getAttributeValues("email");  
+            	String email = (String) emails.get(0);     
+                List lastNames = fetchResp.getAttributeValues("lastName");  
+                String lastName = (String) lastNames.get(0);
+                List firstNames = fetchResp.getAttributeValues("firstName");  
+                String firstName = (String) firstNames.get(0);  
+            }  
+
+         }
+         return verified;
+     
+	 }
+}
